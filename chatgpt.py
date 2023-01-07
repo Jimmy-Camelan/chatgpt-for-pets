303b89a8bdc6b9342f0b9dcab8c7cf0c4d8678ef
Jimmy88 <72346002+Jimmy-Camelan@users.noreply.github.com>
2023-01-07 20:38:58 +0000

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -13,7 +13,6 @@ def openai_create(prompt):
     prompt=prompt,
     temperature=0.9,
     max_tokens=150,
-    top_p=1,
     frequency_penalty=0,
     presence_penalty=0.6,
     stop=[" Human:", " AI:"]
