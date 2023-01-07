8b1c606e1bf79aff123af587d283953e0d6c7bce
Jimmy88 <72346002+Jimmy-Camelan@users.noreply.github.com>
2023-01-07 18:39:16 +0000

    read key from streamlit secrets

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 import os
-open_api_key = os.environ.get('OPENAPI_KEY')
+if os.environ.get("OPENAPI_KEY"):
+  open_api_key = os.environ.get('OPENAPI_KEY')
+else:
+  import streamlit as st
+  open_api_key = st.secrets['OPENAPI_KEY']
