0f37f6cea9beb5208db24c23581e1150dddf6769
Jimmy88 <72346002+Jimmy-Camelan@users.noreply.github.com>
2023-01-17 04:42:36 +0000

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -11,6 +11,30 @@ from config import open_api_key
 import base64
 openai.api_key = open_api_key
 
+import re
+def remove_emojis(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002500-\U00002BEF"  # chinese char
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"  # dingbats
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
+
 def google_auth(sa, sa_private_key):
     SA_str = sa
     info = json.loads(SA_str)
@@ -20,6 +44,7 @@ def google_auth(sa, sa_private_key):
     return client
 
 def text_to_speech(text_to_convert):
+    text_to_convert = remove_emojis(text_to_convert)
     sa = os.environ.get('SERVICE_ACCOUNT')
     sa_private_key = os.environ.get('SA_PRIVATE_KEY')
     client = google_auth(sa, sa_private_key)
